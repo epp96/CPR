cfb8b6cfea9ffecbdce9f200a5356b048024c4b5
Carlos <carmargut@gmail.com>
2017-03-27 16:12:53 +0200

    html test in "botiquin"

--- app/src/main/java/com/example/enrique/cpr/botiquin.java
+++ app/src/main/java/com/example/enrique/cpr/botiquin.java
@@ -1,8 +1,10 @@
 package com.example.enrique.cpr;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.widget.TextView;
 
 /**
@@ -11,32 +13,15 @@ import android.widget.TextView;
 
 public class botiquin extends AppCompatActivity {
     TextView information;
-    String info = "Material de curas e instrumental:\n" +
-            "   - Guantes desechables.\n" +
-            "   - Pinzas.\n" +
-            "   - Termómetro.\n" +
-            "   - Gasas estériles.\n" +
-            "   - Tijeras punta roma.\n" +
-            "   - Esparadrapo.\n" +
-            "   - Tiritas.\n" +
-            "   - Vendas de gasa y elásticas.\n" +
-            "Antisépticos y desinfectantes.\n" +
-            "   - Betadine (povidona yodada).\n" +
-            "   - Cristalmina (Clorhexidina).\n" +
-            "   - Suero fisiológico.\n" +
-            "   - Agua oxigenada.\n" +
-            "Medicamentos\n" +
-            "   - Analgésicos y antitérmicos.\n" +
-            "   - Pomada antiinflamatoria.\n" +
-            "   - Pomada para quemaduras.\n" +
-            "   - Pomada antihistamínica para picaduras.\n" +
-            "Otros\n" +
-            "   - Bolsitas de frio instantáneo.\n";
+    String htmlAsString = getString(R.string.botiquin_contenido_html);
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
         information = (TextView) findViewById(R.id.infoTitle);
-        information.setText(info);
+        information.setText(Html.fromHtml(htmlAsString));
+
     }
 }
