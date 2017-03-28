4875a5aae7793f9d86d6514dde283cdca827111f
Carlos <carmargut@gmail.com>
2017-03-28 11:42:24 +0200

    html test using external files and css.

--- app/src/main/java/com/example/enrique/cpr/botiquin.java
+++ app/src/main/java/com/example/enrique/cpr/botiquin.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 /**
@@ -13,15 +14,32 @@ import android.widget.TextView;
 
 public class botiquin extends AppCompatActivity {
     TextView information;
-    String htmlAsString = getString(R.string.botiquin_contenido_html);
-
-
+    String info = "Material de curas e instrumental:\n" +
+                        "   - Guantes desechables.\n" +
+                        "   - Pinzas.\n" +
+                        "   - Termómetro.\n" +
+                        "   - Gasas estériles.\n" +
+                        "   - Tijeras punta roma.\n" +
+                        "   - Esparadrapo.\n" +
+                        "   - Tiritas.\n" +
+                        "   - Vendas de gasa y elásticas.\n" +
+                        "Antisépticos y desinfectantes.\n" +
+                        "   - Betadine (povidona yodada).\n" +
+                        "   - Cristalmina (Clorhexidina).\n" +
+                        "   - Suero fisiológico.\n" +
+                        "   - Agua oxigenada.\n" +
+                        "Medicamentos\n" +
+                        "   - Analgésicos y antitérmicos.\n" +
+                        "   - Pomada antiinflamatoria.\n" +
+                        "   - Pomada para quemaduras.\n" +
+                        "   - Pomada antihistamínica para picaduras.\n" +
+                        "Otros\n" +
+                        "   - Bolsitas de frio instantáneo.\n";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.info);
-        information = (TextView) findViewById(R.id.infoTitle);
-        information.setText(Html.fromHtml(htmlAsString));
-
-    }
-}
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.test);
+        WebView webview = (WebView) findViewById(R.id.webview_xml);
+        webview.loadUrl("file:///android_asset/botiquin.html");
+        }
+ }
