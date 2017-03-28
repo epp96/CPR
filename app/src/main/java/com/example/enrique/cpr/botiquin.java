b949614809de8e734b7d6f2810aa15c249898a01
Carlos <carmargut@gmail.com>
2017-03-28 11:54:10 +0200

    botiquin is now using html

--- app/src/main/java/com/example/enrique/cpr/botiquin.java
+++ app/src/main/java/com/example/enrique/cpr/botiquin.java
@@ -13,28 +13,7 @@ import android.widget.TextView;
  */
 
 public class botiquin extends AppCompatActivity {
-    TextView information;
-    String info = "Material de curas e instrumental:\n" +
-                        "   - Guantes desechables.\n" +
-                        "   - Pinzas.\n" +
-                        "   - Termómetro.\n" +
-                        "   - Gasas estériles.\n" +
-                        "   - Tijeras punta roma.\n" +
-                        "   - Esparadrapo.\n" +
-                        "   - Tiritas.\n" +
-                        "   - Vendas de gasa y elásticas.\n" +
-                        "Antisépticos y desinfectantes.\n" +
-                        "   - Betadine (povidona yodada).\n" +
-                        "   - Cristalmina (Clorhexidina).\n" +
-                        "   - Suero fisiológico.\n" +
-                        "   - Agua oxigenada.\n" +
-                        "Medicamentos\n" +
-                        "   - Analgésicos y antitérmicos.\n" +
-                        "   - Pomada antiinflamatoria.\n" +
-                        "   - Pomada para quemaduras.\n" +
-                        "   - Pomada antihistamínica para picaduras.\n" +
-                        "Otros\n" +
-                        "   - Bolsitas de frio instantáneo.\n";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
